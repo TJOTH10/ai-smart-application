2bb4027d65bd2cfe4470bd6a6f7a0b7df9adc7e5
chenjl <3608427912@qq.com>
2026-07-19 01:43:34 +0800

    1.开发核心功能，比如用户注册、用户登录、获取登录用户信息、用户注销等 2.全局权限校验AOP+自定义注解

---
+++ src/main/java/com/chenjl/yuaicodemother/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.chenjl.yuaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.chenjl.yuaicodemother.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author Administrator
+ * @since 2026-07-18
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/com/chenjl/yuaicodemother/service/UserService.java
@@ -0,0 +1,91 @@
+package com.chenjl.yuaicodemother.service;
+
+import com.chenjl.yuaicodemother.model.dto.UserQueryRequest;
+import com.chenjl.yuaicodemother.model.vo.LoginUserVO;
+import com.chenjl.yuaicodemother.model.vo.UserVO;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.chenjl.yuaicodemother.model.entity.User;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+
+/**
+ * 用户 服务层。
+ *
+ * @author Administrator
+ * @since 2026-07-18
+ */
+public interface UserService extends IService<User> {
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+
+    /**
+     *
+     * @param userPassword 用户密码
+     * @return 用户加密之后的密码
+     */
+    String getEncryptPassword(String userPassword);
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    /**
+     * 获取用户脱敏后的信息
+     * @param user
+     * @return
+     */
+    UserVO getUserVO(User user);
+
+    /**
+     * 获取用户列表信息
+     * @param userList
+     * @return
+     */
+    List<UserVO> getUserVOList(List<User> userList);
+
+    /**
+     * 根据查询条件构造数据查询参数
+     * @param userQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(UserQueryRequest userQueryRequest);
+}
